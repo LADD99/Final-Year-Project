ed5db9d7dcbe10deff00fdcdd0ef9f631fe02488
benwhite1999 <63452823+benwhite1999@users.noreply.github.com>
2020-04-14 11:20:53 +0100

    Update steggingWithDifferentSizeData.py
    Added error handling to make an easier UX

--- steggingWithDifferentSizeData.py
+++ steggingWithDifferentSizeData.py
@@ -1,17 +1,24 @@
 import stegano
 from stegano import lsb
 import os
+import sys
 
-iDirectory = input("Input Folder: ")  # toSteg
-proc = len(os.listdir(iDirectory))  # get number of files in input folder
-print(proc)  # show number of files that will be processed
-dDirectory = input("Data Folder: ")  # folder where the data to steg is kept #Encrypted
-eData = len(os.listdir(dDirectory))  # number of files in that folder
-print(eData)
-oDirectory = input("Output Folder: ")  # where the stegged images will end up #stegged
+while True:
+    try:
+        iDirectory = input("Input Folder: ")  # toSteg
+        proc = len(os.listdir(iDirectory))  # get number of files in input folder
+        print(proc)  # show number of files that will be processed
+        dDirectory = input("Data Folder: ")  # folder where the data to steg is kept #Encrypted
+        eData = len(os.listdir(dDirectory))  # number of files in that folder
+        print(eData)
+        oDirectory = input("Output Folder: ")  # where the stegged images will end up #stegged
 
-image = 0
-dFile = 0
+        image = 0
+        dFile = 0
+        break
+    except FileNotFoundError:
+        print('Directory not found, Please try again')
+
 
 for x in range(0, eData):  # loop for how many encypted files will be hidden in image set
     dFile = x + 1  # increment to use next data file each loop
@@ -21,9 +28,12 @@ for x in range(0, eData):  # loop for how many encypted files will be hidden in 
     print(pDirectory)
 
     # Loop to process all images in a folder
-    for x in range(0, proc):  # loop for how many images we want to steg
+    try:
+        for x in range(0, proc):  # loop for how many images we want to steg
 
-        image = x + 1  # add one to x to use as file identifier once saved
-        image = str(image)  # convert to string to use in file path
-        secret = lsb.hide(iDirectory + '/' + image + '.bmp', msg)
-        secret.save(pDirectory + '/' + image + ".bmp")
+            image = x + 1  # add one to x to use as file identifier once saved
+            image = str(image)  # convert to string to use in file path
+            secret = lsb.hide(iDirectory + '/' + image + '.bmp', msg)
+            secret.save(pDirectory + '/' + image + ".bmp")
+    except FileNotFoundError:
+        print('Folder not defined')
